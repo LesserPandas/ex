c8ca3d1e89bcf6eed7739ef7c3ea099766e80df8
less3rPanda <86329285+LesserPandas@users.noreply.github.com>
2022-03-27 18:50:36 +0900

    Spring Security 설정 OAuth2 로그인 (구글, 네이버)

--- src/main/java/org/ex/springboot/web/IndexController.java
+++ src/main/java/org/ex/springboot/web/IndexController.java
@@ -1,6 +1,9 @@
 package org.ex.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.ex.springboot.config.auth.LoginUser;
+import org.ex.springboot.config.auth.dto.SessionUser;
 import org.ex.springboot.service.posts.PostsService;
 import org.ex.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +11,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
+
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
